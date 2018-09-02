81afeb7cf01c78c4c1a4642ed4e64e921f48d1ab
Hiratake <hiratake@hiratake.xyz>
2018-09-02 21:52:21 +0900

    Change: ブロック数を計算する部分を関数化

--- src/js/main.js
+++ src/js/main.js
@@ -36,53 +36,11 @@
     var calcChunks = document.getElementById('calcChunks');
     calcChunks.addEventListener('click', function () {
         let result = document.getElementById('chunks');
-        let XpointsValue = [];
-        let ZpointsValue = [];
     
         // 座標情報取得
-        var points = getPoints('point');
-
-        // 面積計算
-        let size = 0.0;
-        let x1 = 0;
-        let x2 = 0;
-        let z1 = 0;
-        let z2 = 0;
-        for (let i = 0; i < points.x.length; i++) {
-            if ((i + 1) >= points.x.length) {
-                x1 = points.x[i];
-                x2 = points.x[0];
-                z1 = points.z[i];
-                z2 = points.z[0];
-            } else {
-                x1 = points.x[i];
-                x2 = points.x[i + 1];
-                z1 = points.z[i];
-                z2 = points.z[i + 1];
-            }
-            size += (x1 * z2) - (x2 * z1);
-        }
-        if (size < 0) {
-            size = size * (-1);
-        }
-        size = size / 2;
-        console.log(size);
-
-        // 辺の長さ計算
-        let side = 0.0;
-        for (let i = 0; i < points.x.length; i++) {
-            if ((i + 1) >= points.x.length) {
-                side = side + Math.abs(points.x[i] - points.x[0]) + Math.abs(points.z[i] - points.z[0]);
-            } else {
-                side = side + Math.abs(points.x[i] - points.x[i + 1]) + Math.abs(points.z[i] - points.z[i + 1]);
-            }
-        }
-        console.log(side);
-
+        const points = getPoints('point');
         // ブロック数計算
-        let blocks = size + (side / 2) + 1;
-        if (blocks == 1) blocks = 0; // ブロック数が1→0に変える。(未指定の場合には1ブロックになってしまうため)
-        console.log(blocks);
+        const blocks = calcBlockNumber(points);
 
         // チャンク数計算
         let chunks = blocks / (16 * 16);
@@ -120,5 +78,60 @@
             points.z.push(Number(data[i + 1].value));
         }
         return points;
+    }
+
+    /**
+     * 座標情報から指定範囲内のブロック数を計算
+     * @param   {Object}  座標情報
+     * @return  {number}  ブロック数
+     */
+    function calcBlockNumber(points) {
+        let size = 0;       // 面積
+        let side = 0;       // 辺の長さ
+        let blocks = 0;     // ブロック数
+
+        let x1 = 0;         // 1点目のX座標値
+        let x2 = 0;         // 2点目のX座標値
+        let z1 = 0;         // 1点目のZ座標値
+        let z2 = 0;         // 2点目のZ座標値
+
+        /* 図形の面積を計算 */
+        for (let i = 0; i < points.x.length; i++) {
+            if ((i + 1) >= points.x.length) {
+                x1 = points.x[i];
+                x2 = points.x[0];
+                z1 = points.z[i];
+                z2 = points.z[0];
+            } else {
+                x1 = points.x[i];
+                x2 = points.x[i + 1];
+                z1 = points.z[i];
+                z2 = points.z[i + 1];
+            }
+            // 外積を計算して加算
+            size += (x1 * z2) - (x2 * z1);
+        }
+        size = size / 2;
+        size = Math.abs(size);
+
+        /* 図形の辺の長さを計算 */
+        for (let i = 0; i < points.x.length; i++) {
+            if ((i + 1) >= points.x.length) {
+                side = side
+                     + Math.abs(points.x[i] - points.x[0])
+                     + Math.abs(points.z[i] - points.z[0]);
+            } else {
+                side = side
+                     + Math.abs(points.x[i] - points.x[i + 1])
+                     + Math.abs(points.z[i] - points.z[i + 1]);
+            }
+        }
+
+        /* ブロック数を計算 */
+        if (size > 0) {
+            // ブロック数 = 面積 + (辺の長さ / 2) + 1
+            blocks = size + (side / 2) + 1;
+        }
+        return blocks;
     }
 })();
